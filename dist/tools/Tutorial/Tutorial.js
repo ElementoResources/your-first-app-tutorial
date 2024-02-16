d7e7167605e5d8a4ff29496d16f4324b5a6de9e2
boocockp <>
2024-02-16 10:44:32 +0000

    Updates from first user test

--- dist/tools/Tutorial/Tutorial.js
+++ dist/tools/Tutorial/Tutorial.js
@@ -1,4 +1,4 @@
-const runtimeUrl = `${window.location.origin}/runtime/runtime.js`
+const runtimeUrl = window.elementoRuntimeUrl || 'https://elemento.online/lib/runtime.js'
 const Elemento = await import(runtimeUrl)
 const {React} = Elemento
 
@@ -9,18 +9,15 @@ function StartPage(props) {
     const {Editor, Preview} = Elemento
     const app = Elemento.useGetObjectState('app')
     const {ShowPage} = app
-    const NextButton_action = React.useCallback(() => {
-        ShowPage(HowtoUsePage)
+    const NextButton_action = React.useCallback(async () => {
+        await ShowPage(HowtoUsePage)
     }, [])
 
     return React.createElement(Page, {id: props.path},
         React.createElement(TextElement, {path: pathWith('Title'), fontSize: 24}, 'Creating your first app with Elemento'),
         React.createElement(TextElement, {path: pathWith('Para1')}, 'Congratulations on deciding to dive in to Elemento!'),
-        React.createElement(TextElement, {path: pathWith('Para2')}, 'This short guide will take you step-by-step through creating a really simple Elemento app, called Hello You.  The app just asks for your name,  and then displays "Hello, Jo" (or whatever you tell it you are called).'),
-        React.createElement(TextElement, {path: pathWith('Para3')}, `You will need:
-- A computer with a good size screen - Full HD (1920 x 1024) is best
-- To be using an up to date version of the Chrome browser
-- About 5 minutes`),
+        React.createElement(TextElement, {path: pathWith('Para2')}, 'This short guide is intended for complete beginners to programming.  It will take you step-by-step through creating a really simple Elemento app, called Hello You.  It will take about 5 minutes.'),
+        React.createElement(TextElement, {path: pathWith('Para3')}, 'You will need a computer with a good size screen (Full HD 1920 x 1080 is best) and an up to date version of the Chrome browse'),
         React.createElement(TextElement, {path: pathWith('Para4')}, 'Let\'s get started - click the Next button below'),
         React.createElement(Button, {path: pathWith('NextButton'), content: 'Next', appearance: 'outline', action: NextButton_action}),
     )
@@ -33,20 +30,47 @@ function HowtoUsePage(props) {
     const {Editor, Preview} = Elemento
     const app = Elemento.useGetObjectState('app')
     const {ShowPage} = app
-    const Previous_action = React.useCallback(() => {
-        ShowPage(StartPage)
+    const Previous_action = React.useCallback(async () => {
+        await ShowPage(StartPage)
     }, [])
-    const NextButton_action = React.useCallback(() => {
-        ShowPage(WhatYouSeePage)
+    const NextButton_action = React.useCallback(async () => {
+        await ShowPage(HowElementoWorksPage)
     }, [])
 
     return React.createElement(Page, {id: props.path},
         React.createElement(TextElement, {path: pathWith('Title'), fontSize: 24}, 'How to Use this Tutorial'),
-        React.createElement(TextElement, {path: pathWith('Para1')}, 'There is a lot to see on this screen - the next page explains what you are looking at.'),
+        React.createElement(TextElement, {path: pathWith('Para1')}, 'This is the Elemento Studio screen - there is a lot to see. The next page explains what you are looking at.'),
         React.createElement(TextElement, {path: pathWith('Para2')}, `Each page contains detailed instructions to complete that step.  
 If you can't see what any instruction means, click the Show Me link beside it.  `),
         React.createElement(TextElement, {path: pathWith('Para3')}, 'You can also click the Help button at the top of the page at any time to find further information about anything you are doing in the tutorial. '),
         React.createElement(TextElement, {path: pathWith('Para4')}, 'You can move around the tutorial with the Previous and Next buttons at the bottom of each page.'),
+        React.createElement(Layout, {path: pathWith('Buttons'), horizontal: true, wrap: false},
+            React.createElement(Button, {path: pathWith('Previous'), content: 'Previous', appearance: 'outline', action: Previous_action}),
+            React.createElement(Button, {path: pathWith('NextButton'), content: 'Next', appearance: 'outline', action: NextButton_action}),
+    ),
+    )
+}
+
+// HowElementoWorksPage.js
+function HowElementoWorksPage(props) {
+    const pathWith = name => props.path + '.' + name
+    const {Page, TextElement, Layout, Button} = Elemento.components
+    const {Editor, Preview} = Elemento
+    const app = Elemento.useGetObjectState('app')
+    const {ShowPage} = app
+    const Previous_action = React.useCallback(async () => {
+        await ShowPage(HowtoUsePage)
+    }, [])
+    const NextButton_action = React.useCallback(async () => {
+        await ShowPage(WhatYouSeePage)
+    }, [])
+
+    return React.createElement(Page, {id: props.path},
+        React.createElement(TextElement, {path: pathWith('Title'), fontSize: 24}, 'How Elemento works - in 30 seconds'),
+        React.createElement(TextElement, {path: pathWith('Para1')}, 'Elemento lets you create apps by assembling parts like pages, buttons, input boxes and pieces of text.  These are called "elements".'),
+        React.createElement(TextElement, {path: pathWith('Para2')}, 'Each element has properties - settings that control what it looks like and what it does.'),
+        React.createElement(TextElement, {path: pathWith('Para3')}, 'Property settings can be plain values, like "green" or 20, or they can be formulas.  Formulas are similar to those in a spreadsheet, and they let the app calculate what to show and do as you use it.'),
+        React.createElement(TextElement, {path: pathWith('Para4')}, 'Now lets see how you set up the elements and their properties.'),
         React.createElement(Layout, {path: pathWith('Buttons'), horizontal: true, wrap: false},
             React.createElement(Button, {path: pathWith('Previous'), content: 'Previous', appearance: 'outline', action: Previous_action}),
             React.createElement(Button, {path: pathWith('NextButton'), content: 'Next', appearance: 'outline', action: NextButton_action}),
@@ -61,24 +85,25 @@ function WhatYouSeePage(props) {
     const {Editor, Preview} = Elemento
     const app = Elemento.useGetObjectState('app')
     const {ShowPage} = app
-    const ShowMe1_action = React.useCallback(() => {
-        Editor.Show('treeItem')
+    const ShowMe1_action = React.useCallback(async () => {
+        await Editor.Show('treeItem')
     }, [])
-    const ShowMe2_action = React.useCallback(() => {
-        Editor.Click('treeItem', 'Main Page')
-        Editor.Show('propertyField')
+    const ShowMe2_action = React.useCallback(async () => {
+        await await Editor.Click('treeItem', 'Main Page')
+        await await Editor.Show('propertiesPanel')
+        await Preview.Show()
     }, [])
-    const ShowMe3_action = React.useCallback(() => {
-        Preview.Show('MainApp')
+    const ShowMe3_action = React.useCallback(async () => {
+        await Preview.Show('MainApp')
     }, [])
-    const ShowMe4_action = React.useCallback(() => {
-        Editor.Show('button')
+    const ShowMe4_action = React.useCallback(async () => {
+        await Editor.Show('menuButton')
     }, [])
-    const Previous_action = React.useCallback(() => {
-        ShowPage(HowtoUsePage)
+    const Previous_action = React.useCallback(async () => {
+        await ShowPage(HowElementoWorksPage)
     }, [])
-    const NextButton_action = React.useCallback(() => {
-        ShowPage(Step1Page)
+    const NextButton_action = React.useCallback(async () => {
+        await ShowPage(Step1Page)
     }, [])
 
     return React.createElement(Page, {id: props.path},
@@ -121,29 +146,29 @@ function Step1Page(props) {
     const {Editor, Preview} = Elemento
     const app = Elemento.useGetObjectState('app')
     const {ShowPage} = app
-    const ShowItem_action = React.useCallback(() => {
-        Editor.Show('treeItem', 'Main Page')
+    const ShowItem_action = React.useCallback(async () => {
+        await Editor.Show('treeItem', 'Main Page')
     }, [])
-    const ExpandItem_action = React.useCallback(() => {
-        Editor.EnsureTreeItemsExpanded('treeItem', 'Main Page')
+    const ExpandItem_action = React.useCallback(async () => {
+        await Editor.EnsureTreeItemsExpanded('treeItem', 'Main Page')
     }, [])
-    const Showlink_action = React.useCallback(() => {
-        Editor.Click('treeItem', 'Title')
+    const Showlink_action = React.useCallback(async () => {
+        await Editor.Click('treeItem', 'Title')
     }, [])
-    const ShowMe4_action = React.useCallback(() => {
-        Editor.Click('propertyField', 'Content')
+    const ShowMe4_action = React.useCallback(async () => {
+        await Editor.Click('propertyField', 'Content')
     }, [])
-    const ShowMe5_action = React.useCallback(() => {
-        Editor.SetValue('propertyField', 'Content', 'Hello You')
+    const ShowMe5_action = React.useCallback(async () => {
+        await Editor.SetValue('propertyField', 'Content', 'Hello You')
     }, [])
-    const ShowMe6_action = React.useCallback(() => {
-        Preview.Show('Title')
+    const ShowMe6_action = React.useCallback(async () => {
+        await Preview.Show('Title')
     }, [])
-    const Previous_action = React.useCallback(() => {
-        ShowPage(WhatYouSeePage)
+    const Previous_action = React.useCallback(async () => {
+        await ShowPage(WhatYouSeePage)
     }, [])
-    const NextButton_action = React.useCallback(() => {
-        ShowPage(Step2Page)
+    const NextButton_action = React.useCallback(async () => {
+        await ShowPage(Step2Page)
     }, [])
 
     return React.createElement(Page, {id: props.path},
@@ -186,29 +211,29 @@ function Step2Page(props) {
     const {Editor, Preview} = Elemento
     const app = Elemento.useGetObjectState('app')
     const {ShowPage} = app
-    const ShowMe21_action = React.useCallback(() => {
-        Editor.Show('treeItem', 'Title')
+    const ShowMe21_action = React.useCallback(async () => {
+        await Editor.Show('treeItem', 'Title')
     }, [])
-    const ShowMe22_action = React.useCallback(() => {
-        Editor.Click('button', 'Insert')
+    const ShowMe22_action = React.useCallback(async () => {
+        await Editor.Click('menuButton', 'Insert')
     }, [])
-    const ShowMe23_action = React.useCallback(() => {
-        Editor.Click('menuItem', 'After')
+    const ShowMe23_action = React.useCallback(async () => {
+        await Editor.Click('menuItem', 'After')
     }, [])
-    const ShowMe24_action = React.useCallback(() => {
-        Editor.Click('menuItem', 'Text Input')
+    const ShowMe24_action = React.useCallback(async () => {
+        await Editor.Click('menuItem', 'Text Input')
     }, [])
-    const ShowMe26_action = React.useCallback(() => {
-        Editor.Click('propertyField', 'Name')
+    const ShowMe26_action = React.useCallback(async () => {
+        await Editor.Click('propertyField', 'Name')
     }, [])
-    const ShowMe25_action = React.useCallback(() => {
-        Editor.SetValue('propertyField', 'Name', 'Your Name')
+    const ShowMe25_action = React.useCallback(async () => {
+        await Editor.SetValue('propertyField', 'Name', 'Your Name[Enter]')
     }, [])
-    const Previous2_action = React.useCallback(() => {
-        ShowPage(Step1Page)
+    const Previous2_action = React.useCallback(async () => {
+        await ShowPage(Step1Page)
     }, [])
-    const Next2_action = React.useCallback(() => {
-        ShowPage(Step3Page)
+    const Next2_action = React.useCallback(async () => {
+        await ShowPage(Step3Page)
     }, [])
 
     return React.createElement(Page, {id: props.path},
@@ -234,7 +259,7 @@ function Step2Page(props) {
             React.createElement(Button, {path: pathWith('ShowMe26'), content: 'Show me', appearance: 'link', action: ShowMe26_action}),
     ),
         React.createElement(Layout, {path: pathWith('Step6'), horizontal: true, wrap: false},
-            React.createElement(TextElement, {path: pathWith('Instruction')}, `Change the Name to 'Your Name'.
+            React.createElement(TextElement, {path: pathWith('Instruction')}, `Change the Name to the words: 'Your Name'.
 You need to press Enter after this change.`),
             React.createElement(Button, {path: pathWith('ShowMe25'), content: 'Show me', appearance: 'link', action: ShowMe25_action}),
     ),
@@ -252,32 +277,32 @@ function Step3Page(props) {
     const {Editor, Preview} = Elemento
     const app = Elemento.useGetObjectState('app')
     const {ShowPage} = app
-    const ShowMe31_action = React.useCallback(() => {
-        Editor.Show('treeItem', 'Your Name')
+    const ShowMe31_action = React.useCallback(async () => {
+        await Editor.Show('treeItem', 'Your Name')
     }, [])
-    const ShowMe32_action = React.useCallback(() => {
-        Editor.Click('button', 'Insert')
+    const ShowMe32_action = React.useCallback(async () => {
+        await Editor.Click('button', 'Insert')
     }, [])
-    const ShowMe33_action = React.useCallback(() => {
-        Editor.Click('menuItem', 'After')
+    const ShowMe33_action = React.useCallback(async () => {
+        await Editor.Click('menuItem', 'After')
     }, [])
-    const ShowMe34_action = React.useCallback(() => {
-        Editor.Click('menuItem', 'Text$')
+    const ShowMe34_action = React.useCallback(async () => {
+        await Editor.Click('menuItem', 'Text$')
     }, [])
-    const ShowMe35_action = React.useCallback(() => {
-        Editor.Click('propertyTypeButton', 'Content')
+    const ShowMe35_action = React.useCallback(async () => {
+        await Editor.Click('propertyTypeButton', 'Content')
     }, [])
-    const ShowMe36_action = React.useCallback(() => {
-        Editor.SetValue('propertyField', 'Content', "'Hello there, ' + YourName")
+    const ShowMe36_action = React.useCallback(async () => {
+        await Editor.SetValue('propertyField', 'Content', '\'Hello there, \' + YourName')
     }, [])
-    const ShowMe37_action = React.useCallback(() => {
-        Preview.SetValue('YourName', 'Alexandra')
+    const ShowMe37_action = React.useCallback(async () => {
+        await Preview.SetValue('YourName', 'Alexandra')
     }, [])
-    const Previous3_action = React.useCallback(() => {
-        ShowPage(Step2Page)
+    const Previous3_action = React.useCallback(async () => {
+        await ShowPage(Step2Page)
     }, [])
-    const Next3_action = React.useCallback(() => {
-        ShowPage(FinishPage)
+    const Next3_action = React.useCallback(async () => {
+        await ShowPage(FinishPage)
     }, [])
 
     return React.createElement(Page, {id: props.path},
@@ -304,7 +329,7 @@ function Step3Page(props) {
 It should change to FX= (for a Formula).  Don't worry about the red error message.`),
             React.createElement(Button, {path: pathWith('ShowMe35'), content: 'Show me', appearance: 'link', action: ShowMe35_action}),
     ),
-        React.createElement(Layout, {path: pathWith('Step6'), horizontal: true, wrap: false},
+        React.createElement(Layout, {path: pathWith('Step6'), horizontal: true, wrap: true},
             React.createElement(TextElement, {path: pathWith('Instruction')}, `Change the Content formula to exactly what is on the next line:
  'Hello there, ' + YourName`),
             React.createElement(Button, {path: pathWith('ShowMe36'), content: 'Show me', appearance: 'link', action: ShowMe36_action}),
@@ -314,7 +339,6 @@ It should change to FX= (for a Formula).  Don't worry about the red error messag
 `),
             React.createElement(Button, {path: pathWith('ShowMe37'), content: 'Show me', appearance: 'link', action: ShowMe37_action}),
     ),
-        React.createElement(TextElement, {path: pathWith('Step7')}, 'Your text here'),
         React.createElement(Layout, {path: pathWith('Buttons'), horizontal: true, wrap: false},
             React.createElement(Button, {path: pathWith('Previous3'), content: 'Previous', appearance: 'outline', action: Previous3_action}),
             React.createElement(Button, {path: pathWith('Next3'), content: 'Next', appearance: 'outline', action: Next3_action}),
@@ -329,17 +353,18 @@ function FinishPage(props) {
     const {Editor, Preview} = Elemento
     const app = Elemento.useGetObjectState('app')
     const {ShowPage} = app
-    const Previous_action = React.useCallback(() => {
-        ShowPage(Step3Page)
+    const Previous_action = React.useCallback(async () => {
+        await ShowPage(Step3Page)
     }, [])
-    const BacktoStartButton_action = React.useCallback(() => {
-        ShowPage(StartPage)
+    const BacktoStartButton_action = React.useCallback(async () => {
+        await ShowPage(StartPage)
     }, [])
 
     return React.createElement(Page, {id: props.path},
         React.createElement(TextElement, {path: pathWith('Title'), fontSize: 24}, 'You\'ve got there!'),
         React.createElement(TextElement, {path: pathWith('Para1')}, 'Congratulations - you have built your first app with Elemento!  It doesn\'t do very much, but it is a complete working program.'),
         React.createElement(TextElement, {path: pathWith('Para2')}, 'This tutorial was intended to give you a flavour of using Elemento, without too much explanation.  Other tutorials will go deeper into why you were doing each step.'),
+        React.createElement(TextElement, {path: pathWith('Para2b')}, 'If you would like to, please play around with any of the elements and properties, just to see what happens.  You can always start afresh just by downloading this tutorial again'),
         React.createElement(TextElement, {path: pathWith('Para3')}, 'More tutorials and examples will appear regularly to help you improve your skills with Elemento. You can also explore the Help to find a detailed explanation of anything you need to know. '),
         React.createElement(TextElement, {path: pathWith('Para4')}, 'Thanks for trying Elemento - enjoy the rest of your journey!'),
         React.createElement(Layout, {path: pathWith('Buttons'), horizontal: true, wrap: false},
@@ -354,11 +379,11 @@ export default function Tutorial(props) {
     const pathWith = name => 'Tutorial' + '.' + name
     const {App} = Elemento.components
     const {Editor, Preview} = Elemento
-    const pages = {StartPage, HowtoUsePage, WhatYouSeePage, Step1Page, Step2Page, Step3Page, FinishPage}
+    const pages = {StartPage, HowtoUsePage, HowElementoWorksPage, WhatYouSeePage, Step1Page, Step2Page, Step3Page, FinishPage}
     const {appContext} = props
     const app = Elemento.useObjectState('app', new App.State({pages, appContext}))
-    const Tutorial_startupAction = React.useCallback(() => {
-        Editor.SetOptions({showBeforeActions: true,
+    const Tutorial_startupAction = React.useCallback(async () => {
+        await Editor.SetOptions({showBeforeActions: true,
         showWithPointer: true, delay: 250})
     }, [])
 
